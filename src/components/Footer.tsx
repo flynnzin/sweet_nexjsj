ef130770d7c297dd8b25ebe2f7e7a9826d58c8bd
Cristopher Leonardo <Lzfts593@gmail.com>
2024-11-27 01:39:52 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,12 +26,9 @@ export const Footer = () => {
                         onBackground="neutral-weak">
                         © {currentYear} /
                     </Text>
-                    <Text paddingX="4">
-                        {person.name}
-                    </Text>
                     <Text onBackground="neutral-weak">
                         {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-                        / Build your portfolio with <SmartLink style={{marginLeft: '-0.125rem'}} href="https://once-ui.com/templates/magic-portfolio">Once UI</SmartLink>
+                        / TesteFooster <SmartLink style={{marginLeft: '-0.125rem'}} href="https://once-ui.com/templates/magic-portfolio">Once UI</SmartLink>
                     </Text>
                 </Text>
                 <Flex
