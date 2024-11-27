e0a386378cd699a0d00db0d30f85dfc2fc1fdcf7
Cristopher Leonardo <Lzfts593@gmail.com>
2024-11-27 01:15:11 -0300

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -41,7 +41,7 @@ const effects = {
 
 const style = {
     theme:       'dark',         // dark | light
-    neutral:     'gray',         // sand | gray | slate
+    neutral:     'slate',         // sand | gray | slate
     brand:       'pink',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     accent:      'pink',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     solid:       'contrast',     // color | contrast
