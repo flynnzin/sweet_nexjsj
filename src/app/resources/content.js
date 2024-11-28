dbaa64c5c01be020805f045967621b6f33a855bc
Cristopher Leonardo <Lzfts593@gmail.com>
2024-11-27 22:18:29 -0300

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -156,23 +156,18 @@ const blog = {
     label: 'Atualizações',
     title: 'Atualização sobre o servidor e os design',
     description: `Read what ${person.name} has been up to recently`
-    // Create new blog posts by adding a new .mdx file to app/blog/posts
-    // All posts will be listed on the /blog route
 }
 
 const work = {
     label: 'Trabalhos',
     title: 'My projects',
     description: `Designers Desenvolvidos`
-    // Create new project pages by adding a new .mdx file to app/blog/posts
-    // All projects will be listed on the /home and /work routes
 }
 
 const gallery = {
     label: 'Imagens',
     title: 'My photo gallery',
     description: `A photo collection by ${person.name}`, // horizontal - vertical
-    // Images from https://pexels.com
     images: [
         { 
             src: '/images/gallery/01.png', 
